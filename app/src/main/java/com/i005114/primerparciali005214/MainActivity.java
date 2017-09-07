8e8b65c32e494596e47b7e7ddec6eb7f198b3cc7
christian <christiancamilo1221@gmail.com>
2017-09-07 13:57:56 -0500

    dd

--- app/src/main/java/com/i005114/primerparciali005214/MainActivity.java
+++ app/src/main/java/com/i005114/primerparciali005214/MainActivity.java
@@ -1,7 +1,13 @@
 package com.i005114.primerparciali005214;
 
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.bnv_1);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_1:
+                        LinearFragment unoFragment = new  LinearFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1, unoFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_2:
+                      RelativeFragment dosFragment = new RelativeFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1, dosFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 2", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+        });
     }
-}
+    }
+
---
+++ app/src/main/java/com/i005114/primerparciali005214/RelativeFragment.java
@@ -0,0 +1,29 @@
+package com.i005114.primerparciali005214;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RelativeFragment extends Fragment {
+
+
+    public RelativeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_relative, container, false);
+    }
+
+}
